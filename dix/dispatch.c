b1b0521af9459b4ebea714dc0fc6c724b708aca9
Enrico Weigelt, metux IT consult <info@metux.net>
2024-02-16 17:25:29 +0100

    fix name clash on 'GC' between Xlib and Xserver
    Both xlib as well as the Xserver use the same identifier "GC" for
    different types. While on xlib it's just the numerical ID of a GC,
    the xserver defines a struct for it by the same name. This is this
    ugly and needs ridiculous hacks for Xserver code that needs xlib.
    
    Easy to solve by just renaming the GC typedef to GCRec (consistent
    with how we're naming other structs) and replacing GC* by GCPtr.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/dispatch.c
+++ dix/dispatch.c
@@ -1553,7 +1553,7 @@ int
 ProcCreateGC(ClientPtr client)
 {
     int error, rc;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
     unsigned len;
 
@@ -1570,7 +1570,7 @@ ProcCreateGC(ClientPtr client)
     len = client->req_len - bytes_to_int32(sizeof(xCreateGCReq));
     if (len != Ones(stuff->mask))
         return BadLength;
-    pGC = (GC *) CreateGC(pDraw, stuff->mask, (XID *) &stuff[1], &error,
+    pGC = (GCPtr) CreateGC(pDraw, stuff->mask, (XID *) &stuff[1], &error,
                           stuff->gc, client);
     if (error != Success)
         return error;
@@ -1582,7 +1582,7 @@ ProcCreateGC(ClientPtr client)
 int
 ProcChangeGC(ClientPtr client)
 {
-    GC *pGC;
+    GCPtr pGC;
     int result;
     unsigned len;
 
@@ -1603,8 +1603,8 @@ ProcChangeGC(ClientPtr client)
 int
 ProcCopyGC(ClientPtr client)
 {
-    GC *dstGC;
-    GC *pGC;
+    GCPtr dstGC;
+    GCPtr pGC;
     int result;
 
     REQUEST(xCopyGCReq);
@@ -1628,7 +1628,7 @@ ProcCopyGC(ClientPtr client)
 int
 ProcSetDashes(ClientPtr client)
 {
-    GC *pGC;
+    GCPtr pGC;
     int result;
 
     REQUEST(xSetDashesReq);
@@ -1654,7 +1654,7 @@ int
 ProcSetClipRectangles(ClientPtr client)
 {
     int nr, result;
-    GC *pGC;
+    GCPtr pGC;
 
     REQUEST(xSetClipRectanglesReq);
 
@@ -1679,7 +1679,7 @@ ProcSetClipRectangles(ClientPtr client)
 int
 ProcFreeGC(ClientPtr client)
 {
-    GC *pGC;
+    GCPtr pGC;
     int rc;
 
     REQUEST(xResourceReq);
@@ -1769,7 +1769,7 @@ ProcCopyArea(ClientPtr client)
 {
     DrawablePtr pDst;
     DrawablePtr pSrc;
-    GC *pGC;
+    GCPtr pGC;
 
     REQUEST(xCopyAreaReq);
     RegionPtr pRgn;
@@ -1807,7 +1807,7 @@ int
 ProcCopyPlane(ClientPtr client)
 {
     DrawablePtr psrcDraw, pdstDraw;
-    GC *pGC;
+    GCPtr pGC;
 
     REQUEST(xCopyPlaneReq);
     RegionPtr pRgn;
@@ -1853,7 +1853,7 @@ int
 ProcPolyPoint(ClientPtr client)
 {
     int npoint;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xPolyPointReq);
@@ -1876,7 +1876,7 @@ int
 ProcPolyLine(ClientPtr client)
 {
     int npoint;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xPolyLineReq);
@@ -1899,7 +1899,7 @@ int
 ProcPolySegment(ClientPtr client)
 {
     int nsegs;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xPolySegmentReq);
@@ -1919,7 +1919,7 @@ int
 ProcPolyRectangle(ClientPtr client)
 {
     int nrects;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xPolyRectangleReq);
@@ -1940,7 +1940,7 @@ int
 ProcPolyArc(ClientPtr client)
 {
     int narcs;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xPolyArcReq);
@@ -1960,7 +1960,7 @@ int
 ProcFillPoly(ClientPtr client)
 {
     int things;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xFillPolyReq);
@@ -1990,7 +1990,7 @@ int
 ProcPolyFillRectangle(ClientPtr client)
 {
     int things;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xPolyFillRectangleReq);
@@ -2012,7 +2012,7 @@ int
 ProcPolyFillArc(ClientPtr client)
 {
     int narcs;
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
 
     REQUEST(xPolyFillArcReq);
@@ -2085,7 +2085,7 @@ ReformatImage(char *base, int nbytes, int bpp, int order)
 int
 ProcPutImage(ClientPtr client)
 {
-    GC *pGC;
+    GCPtr pGC;
     DrawablePtr pDraw;
     long length;                /* length of scanline server padded */
     long lengthProto;           /* length of scanline protocol padded */
@@ -2356,7 +2356,7 @@ ProcPolyText(ClientPtr client)
 
     REQUEST(xPolyTextReq);
     DrawablePtr pDraw;
-    GC *pGC;
+    GCPtr pGC;
 
     REQUEST_AT_LEAST_SIZE(xPolyTextReq);
     VALIDATE_DRAWABLE_AND_GC(stuff->drawable, pDraw, DixWriteAccess);
@@ -2380,7 +2380,7 @@ ProcImageText8(ClientPtr client)
 {
     int err;
     DrawablePtr pDraw;
-    GC *pGC;
+    GCPtr pGC;
 
     REQUEST(xImageTextReq);
 
@@ -2406,7 +2406,7 @@ ProcImageText16(ClientPtr client)
 {
     int err;
     DrawablePtr pDraw;
-    GC *pGC;
+    GCPtr pGC;
 
     REQUEST(xImageTextReq);
 
