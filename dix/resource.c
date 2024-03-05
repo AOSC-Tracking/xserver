11fd0b9c7691c58224df304e283e480d79c2b61b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-03-05 11:01:17 +0100

    dix: unexport DeletePassiveGrab()
    This function isn't used by any driver/module, so no need to export it.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/resource.c
+++ dix/resource.c
@@ -123,6 +123,7 @@ Equipment Corporation.
 
 #include <X11/X.h>
 
+#include "dix/dixgrabs_priv.h"
 #include "dix/gc_priv.h"
 #include "dix/registry_priv.h"
 #include "os/osdep.h"
@@ -136,7 +137,6 @@ Equipment Corporation.
 #include "dixfont.h"
 #include "colormap.h"
 #include "inputstr.h"
-#include "dixevents.h"
 #include "dixgrabs.h"
 #include "cursor.h"
 #ifdef PANORAMIX
