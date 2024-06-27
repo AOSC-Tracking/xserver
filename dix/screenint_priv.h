letoram/xarcan

commit 1277bb7143b8c62842384d5947c35129c31a1e9f

dix: unexport AttachOutputGPU() and DetachOutputGPU()
These aren't externally used, thus no need to export them.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1349>

dix/screenint_priv.h
@@ -20,4 +20,7 @@ void RemoveGPUScreen(ScreenPtr pScreen);
 void AttachUnboundGPU(ScreenPtr pScreen, ScreenPtr newScreen);
 void DetachUnboundGPU(ScreenPtr unbound);
 
+void AttachOutputGPU(ScreenPtr pScreen, ScreenPtr newScreen);
+void DetachOutputGPU(ScreenPtr output);
+
 #endif /* _XSERVER_DIX_SCREENINT_PRIV_H */

include/screenint.h
@@ -55,12 +55,6 @@ typedef struct _Depth *DepthPtr;
 typedef struct _Screen *ScreenPtr;
 
 extern _X_EXPORT void
-AttachOutputGPU(ScreenPtr pScreen, ScreenPtr newScreen);
-
-extern _X_EXPORT void
-DetachOutputGPU(ScreenPtr output);
-
-extern _X_EXPORT void
 AttachOffloadGPU(ScreenPtr pScreen, ScreenPtr newScreen);
 
 extern _X_EXPORT void

AOSC-Tracking/xserver

commit c3df1552b4f2a44477ed1971af0abc65e28763e8

dix: re-export AttachOutputGPU() and DetachOutputGPU()
Nvidia driver needs them for PRIME handling.

Fixes: 1277bb7143b8c62842384d5947c35129c31a1e9f.

dix/screenint_priv.h
@@ -20,9 +20,6 @@ void RemoveGPUScreen(ScreenPtr pScreen);
 void AttachUnboundGPU(ScreenPtr pScreen, ScreenPtr newScreen);
 void DetachUnboundGPU(ScreenPtr unbound);
 
-void AttachOutputGPU(ScreenPtr pScreen, ScreenPtr newScreen);
-void DetachOutputGPU(ScreenPtr output);
-
 void AttachOffloadGPU(ScreenPtr pScreen, ScreenPtr newScreen);
 void DetachOffloadGPU(ScreenPtr slave);
 

include/screenint.h
@@ -53,6 +53,10 @@ typedef struct _PixmapFormat *PixmapFormatPtr;
 typedef struct _Visual *VisualPtr;
 typedef struct _Depth *DepthPtr;
 typedef struct _Screen *ScreenPtr;
+
+extern _X_EXPORT void AttachOutputGPU(ScreenPtr pScreen, ScreenPtr newScreen);
+extern _X_EXPORT void DetachOutputGPU(ScreenPtr output);
+
 typedef struct _ColormapRec *ColormapPtr;
 
 #endif                          /* SCREENINT_H */